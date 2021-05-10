9a75e1a9bb7d499ceea24cb162db3a11cb818342
koushikeaswar <koushikeasward@gmail.com>
2021-05-10 20:36:37 +0530

    Live update by DUKE

--- app/src/main/java/com/mycafe/admin/MainActivity.java
+++ app/src/main/java/com/mycafe/admin/MainActivity.java
@@ -1,19 +1,16 @@
 package com.mycafe.admin;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -21,69 +18,51 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     FirebaseFirestore fStore;
-    String fooditems, totalcost;
-    FirebaseAuth fAuth;
-    FirebaseUser user;
-    String userId;
+    ArrayList<items> datalist;
+    RecyclerView recyclerView;
+    RecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.reyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        datalist = new ArrayList<>();
+        adapter = new RecyclerAdapter(datalist);
+        recyclerView.setAdapter(adapter);
+
         fStore = FirebaseFirestore.getInstance();
+        fStore.collection("orders").get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    ArrayList<DocumentSnapshot> list1 = (ArrayList<DocumentSnapshot>) queryDocumentSnapshots.getDocuments();
+                    for (DocumentSnapshot d : list1){
+                        items obj = d.toObject(items.class);
+                        datalist.add(obj);
+                    }
+                    adapter.notifyDataSetChanged();
+                    refresh();
+                });
 
+    }
 
+    private void refresh() {
+        fStore.collection("orders").addSnapshotListener((value, error) -> {
+            if (error!=null) {
+                Log.w("TAG", "Listen Failed", error);
+                return;
+            }
 
-
-
-        CollectionReference cb = fStore.collection("orders");
-        cb.addSnapshotListener(MainActivity.this, new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                //System.out.println(value.getDocuments());
-                refresh();
-               // Toast.makeText(MainActivity.this, "" + value.getDocuments(), Toast.LENGTH_SHORT).show();
+            for (DocumentChange doc : value.getDocumentChanges()) {
+                datalist.add(doc.getDocument().toObject(items.class));
+                adapter.notifyDataSetChanged();
             }
         });
-
-        /*DocumentReference documentReference = fStore.collection("orders").document(userId);
-        documentReference.addSnapshotListener(MainActivity.this, new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
-                fooditems = documentSnapshot.getString("foodorder");
-                totalcost = documentSnapshot.getString("totalcost");
-
-                //textView.setText(data);
-                Toast.makeText(MainActivity.this, "" + fooditems + totalcost, Toast.LENGTH_SHORT).show();
-            }
-        });*/
-    }
-    private void refresh(){
-
-        fStore.collection("orders").
-                get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                userId=document.getId();
-
-//Koushik, document.get("enter the field name") all the specific field oda data will be there.If you want to show entire document na use document.getData()
-//so youtube la documents ellam epdi recyclerview la kamikarathu paaru
-
-                                //here is the example for both
-                                Toast.makeText(MainActivity.this, ""+document.get("date"), Toast.LENGTH_SHORT).show();
-
-                                Toast.makeText(MainActivity.this, ""+document.getData(), Toast.LENGTH_SHORT).show();
-                                Log.d("TAG", document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            Log.d("TAG", "Error getting documents: ", task.getException());
-                        }
-                    }
-                });
     }
 }
